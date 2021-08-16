8985efa3593e3b403c07c0b6938a3716218cbb41
Tianle Xu <xtlsoft@126.com>
2021-08-16 22:54:58 +0800

    Update helloworld.ts

--- src/assets/examples/helloworld.ts
+++ src/assets/examples/helloworld.ts
@@ -1,5 +1,10 @@
-export default `let main = fn() {
-  print("Hello, world");
-};
+export default `!import playground/println.f5w
+!import playground/help.f5w
 
-main();`
+showHelpText();
+
+println("\r\n");
+
+println("You can use print(\"xxx\"); to output strings.");
+println("When \"playground/println.f5w\" is imported, then you can use println.");
+`
